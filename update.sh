6cbd87fed992db0d59414f2049756e0ff7911012
Andy Botting <andy@andybotting.com>
2016-12-09 08:44:35 +1100

    Add update script

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for PKG in $(find . -name PKGBUILD); do
+    . $PKG
+    echo $pkgname
+    echo $pkgver
+
+    # Fetch latest tag
+    url=$(echo $source | sed 's/git+\(.*\)#tag.*/\1/')
+    newver=$(git ls-remote --tags $url | grep -v '{}' | awk -F/ '{print $NF}' | grep -E '^[[:digit:]]\.' | sort -V | tail -n1)
+    echo $newver
+
+    if [[ $pkgver != $newver ]]; then
+        echo "Updating $pkgname..."
+        sed -i "s/pkgver=.*/pkgver='$newver'/g" $PKG
+    fi
+done
