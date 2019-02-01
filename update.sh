56d8bfc1fe3dccde6e4e0a2d3437cff4342f7d22
Andy Botting <andy@andybotting.com>
2019-02-01 16:03:45 +1100

    Update colours

--- update.sh
+++ update.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+message() {
+    echo -e "\e[1;32m==>\e[39m ${1}\e[0m"
+}
+
+info() {
+    echo -e "\e[1;34m::\e[39m ${1}\e[0m"
+}
+
+error() {
+    echo -e "\e[1;31m==> ERROR:\e[39m ${1}\e[0m"
+}
+
 if [ -z "$1" ]; then
     PKGBUILDS=$(find . -maxdepth 2 -type f -name PKGBUILD)
 else
@@ -12,27 +24,27 @@ source=''
 
 for PKG in $PKGBUILDS; do
     . $PKG
-    echo -e "\e[1;32m==> \e[39mChecking package: $pkgname\e[0m"
-    echo -e "\e[1;34m:: \e[39mcurrent => $pkgver\e[0m"
+    message "Checking package: $pkgname"
+    info "current => $pkgver"
 
     # Fetch latest tag
     if [[ "$source" =~ /github.com/ ]]; then
         repo=$(echo $source | sed -e 's,.*://github.com/\([^/]*\)/\([^/]*\)/.*,\1/\2,g')
         latest=$(curl -s https://api.github.com/repos/$repo/tags | jq -r '.[].name' | grep -E '^(v?)[[:digit:]]{0,3}\.' | sed 's/^v//g' | sort -V | tail -n1)
     else
-        echo -e "\e[1;31m==> ERROR:\e[39m Can't handle source: $source\e[0m"
+        error "Can't handle source: $source"
         continue
     fi
 
     [ -z $latest ] && break
-    echo -e "\e[1;34m:: \e[39mlatest => $latest\e[0m"
+    info "latest => $latest"
 
     if [[ "$pkgver" != "$latest" ]]; then
         src="https://github.com/${repo}/archive/${latest}.tar.gz"
-        echo -e "\e[1;34m:: \e[39mFetching sha512sum...\e[0m"
+        info "Fetching sha512sum..."
         sha512sum=$(curl -s -L "$src" | sha512sum | awk '{print $1}')
 
-        echo "Updating $pkgname..."
+        message "Updating $pkgname..."
         (
             cd "$(dirname $PKG)" || exit
             git checkout master
@@ -40,11 +52,11 @@ for PKG in $PKGBUILDS; do
             git add .
             git -c pager.diff=false diff --cached
             if sudo ccm64 s; then
-                echo -e "\e[1;32m==>\e[39m Looks good.. SHIP IT!\e[0m"
+                message "Looks good.. SHIP IT!"
                 git commit -m "Update to v${latest}-1"
                 git push
             else
-                echo -e "\e[1;31m==> ERROR:\e[39m Failed to build package. Resetting.\e[0m"
+                error "Failed to build package. Resetting."
                 git reset --hard
             fi
         )
